112b58ebabecc09658029e51daae469d59df5525
Kolya <nikzigalov@gmail.com>
2024-06-15 16:09:44 +0300

    HW5_1

--- HW5/hw_window_functions.sql
+++ HW5/hw_window_functions.sql
@@ -38,7 +38,7 @@ USE WideWorldImporters
 Нарастающий итог должен быть без оконной функции.
 */
 
-set statistics time, io on;
+--set statistics time, io on;
 
 with t as (
 SELECT  
@@ -59,18 +59,21 @@ WHERE
 	and	i.CustomerID = 4
 GROUP BY 
 	i.InvoiceID, i.CustomerID, c.CustomerName, i.InvoiceDate
+--ORDER BY i.InvoiceDate
 ),
 t1 as (
 	select *,
 	(select SUM(Tsum)  from 
-						(select CustomerID, EOMONTH(InvoiceDate) InvoiceDate, AVG(TotalMonthSaleSum) as TSum from t
+						(select CustomerID, EOMONTH(InvoiceDate) InvoiceDate, AVG(TotalMonthSaleSum) as TSum 
+						from t
 						group by CustomerID, EOMONTH(InvoiceDate)) q1  where q.CustomerID = q1.CustomerID and EOMONTH(q.InvoiceDate) >= EOMONTH(q1.InvoiceDate)) as TQSum
 	from 
-			(select CustomerID, EOMONTH(InvoiceDate) InvoiceDate, AVG(TotalMonthSaleSum) as TSum from t
+			(select CustomerID, EOMONTH(InvoiceDate) InvoiceDate, AVG(TotalMonthSaleSum) as TSum 
+			from t
 			group by CustomerID, EOMONTH(InvoiceDate)) q
 
 )
-SELECT t.*, t1.TQSum
+SELECT t.InvoiceID, t.CustomerName, t.InvoiceDate, t.InvoiceSaleSum, t1.TQSum as [Нарастающий итог по месяцу]
 FROM t
 JOIN t1 on t.CustomerID = t1.CustomerID and EOMONTH(t.InvoiceDate) = EOMONTH(t1.InvoiceDate)
 ORDER BY t.CustomerID, t.InvoiceDate
@@ -95,7 +98,7 @@ GROUP BY
 	i.InvoiceID, i.CustomerID, c.CustomerName, i.InvoiceDate
 )
 SELECT 
-	*, 
+	InvoiceID, CustomerName, InvoiceDate, 
 	SUM(InvoiceSaleSum) OVER (PARTITION BY CustomerID ORDER BY EOMONTH(InvoiceDate)) as TotalMonthSaleSum
 FROM a
 ORDER BY CustomerID, InvoiceDate
@@ -107,79 +110,80 @@ ORDER BY CustomerID, InvoiceDate
 в каждом месяце за 2016 год (по 2 самых популярных продукта в каждом месяце).
 */
 
+-- комментарий к ДЗ "взять из таблицы, а не рассчитывать.." не понятен. Что вы имеете в виду? что из какой таблицы нужно взять? Как без рассчета?
+-- новый вариант через количество проданных экземпляров продукта, хотя в задании выше написано "популярных продуктов"
 WITH a as (
-SELECT  
-	EOMONTH(i.InvoiceDate) as [Period], il.[Description], COUNT(il.StockItemID) as GoodsCount
-	,dense_rank() OVER(PARTITION BY EOMONTH(i.InvoiceDate) ORDER BY COUNT(il.StockItemID) desc) as GoodsRating
+SELECT 
+EOMONTH(i.InvoiceDate) as [Period], il.[Description], il.StockItemID, SUM(il.Quantity) as TotalQuantity,
+dense_rank() OVER(PARTITION BY EOMONTH(i.InvoiceDate) ORDER BY SUM(il.Quantity) desc) as GoodsRating
 FROM 
 	[Sales].[Invoices] i
 JOIN [Sales].[InvoiceLines] il on i.InvoiceID = il.InvoiceID
 WHERE 
 	i.InvoiceDate BETWEEN '2016-01-01' and '2016-12-31'
-GROUP BY 
-	EOMONTH(i.InvoiceDate), il.Description, il.StockItemID
---ORDER BY EOMONTH(i.InvoiceDate),  COUNT(il.StockItemID) desc
+GROUP BY EOMONTH(i.InvoiceDate), il.[Description], il.StockItemID
+--ORDER BY SUM(il.Quantity) desc
+)
+select Period, Description, TotalQuantity, GoodsRating
+from a
+WHERE GoodsRating <=2
+;
+
+
+--старый вариант в котором подсчитана частота заказа уникального продукта
+WITH a as (
+select EOMONTH(i.InvoiceDate) as [Period], il.[Description], il.StockItemID, COUNT(il.StockItemID) as GoodsCount,
+dense_rank() OVER(PARTITION BY EOMONTH(i.InvoiceDate) ORDER BY COUNT(il.StockItemID) desc) as GoodsRating
+FROM 
+	[Sales].[Invoices] i
+JOIN [Sales].[InvoiceLines] il on i.InvoiceID = il.InvoiceID
+WHERE 
+	i.InvoiceDate BETWEEN '2016-01-01' and '2016-12-31'
+GROUP BY EOMONTH(i.InvoiceDate), il.[Description] , il.StockItemID
+--ORDER BY EOMONTH(i.InvoiceDate)
 )
 SELECT 
 	Period, Description, GoodsCount, GoodsRating
 FROM a
 WHERE GoodsRating <=2
 ORDER BY [Period], GoodsCount desc
-
+;
 /*
 4. Функции одним запросом
 Посчитайте по таблице товаров (в вывод также должен попасть ид товара, название, брэнд и цена):
 * пронумеруйте записи по названию товара, так чтобы при изменении буквы алфавита нумерация начиналась заново
 * посчитайте общее количество товаров и выведете полем в этом же запросе
 * посчитайте общее количество товаров в зависимости от первой буквы названия товара
+* отобразите следующий id товара исходя из того, что порядок отображения товаров по имени 
+* предыдущий ид товара с тем же порядком отображения (по имени)
+* названия товара 2 строки назад, в случае если предыдущей строки нет нужно вывести "No items"
+* сформируйте 30 групп товаров по полю вес товара на 1 шт
+Для этой задачи НЕ нужно писать аналог без аналитических функций.
 */
-SELECT	
+
+SELECT
 	[StockItemID]
 	,[StockItemName]
 	,[Brand]
 	,[UnitPrice]
-	,TotalGoodsCount
-	,count([StockItemName]) over(order by lnum) as lnumGoodsCount
-FROM
-	(SELECT  [StockItemID]
-      ,[StockItemName]
-	  ,Brand
-      ,[UnitPrice]
-	  ,dense_rank () over ( order by LEFT([StockItemName],1)) as lnum
-	  ,(select count([StockItemName]) from [Warehouse].[StockItems]) AS TotalGoodsCount
-  FROM [WideWorldImporters].[Warehouse].[StockItems] ) as q
-
-/*
-* отобразите следующий id товара исходя из того, что порядок отображения товаров по имени 
-* предыдущий ид товара с тем же порядком отображения (по имени)
-* названия товара 2 строки назад, в случае если предыдущей строки нет нужно вывести "No items"*/
-
-SELECT  [StockItemID]
-	  ,lead([StockItemID],1) over ( order by [StockItemName]) as NextStockId
-	  ,lag([StockItemID],1) over ( order by [StockItemName]) as PrevStockId
-      ,[StockItemName]
-	  ,[Brand]
+	,dense_rank () over ( order by LEFT([StockItemName],1)) as lnum
+	,(select count([StockItemName]) from [Warehouse].[StockItems]) AS TotalGoodsCount
+	,count([StockItemName]) over(partition by left([StockItemName],1) ) as lnumGoodsCount
+	,lead([StockItemID],1) over ( order by [StockItemName]) as NextStockId
+	,lag([StockItemID],1) over ( order by [StockItemName]) as PrevStockId
 	  ,CASE 
 		WHEN lag([StockItemName],2) over(order by [StockItemName]) IS NULL THEN 'No items' 
 		ELSE lag([StockItemName],2) over(order by [StockItemName]) END as PrevName
-      ,[UnitPrice]
-  FROM [WideWorldImporters].[Warehouse].[StockItems] 
-  ORDER BY 
-  [StockItemName]
-
-
-/*
---* сформируйте 30 групп товаров по полю вес товара на 1 шт
-Для этой задачи НЕ нужно писать аналог без аналитических функций.
-*/
-
-
-SELECT  [StockItemID]
-      ,[StockItemName]
-      ,TypicalWeightPerUnit
-	  ,ntile(30) over (order by TypicalWeightPerUnit) as WeightGroup
-  FROM [WideWorldImporters].[Warehouse].[StockItems]
-
+	 ,ntile(30) over (order by TypicalWeightPerUnit) as WeightGroup
+FROM
+	[WideWorldImporters].[Warehouse].[StockItems]
+GROUP BY 
+	[StockItemID]
+	,[StockItemName]
+	,[Brand]
+	,[UnitPrice]
+	,TypicalWeightPerUnit
+ORDER BY [StockItemName]
 
 
 /*
